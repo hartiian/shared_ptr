7c7c0599df6d57b911ae4b9c7ce420de62c661bc
Arpine Harutyunyan <159726808+hartiian@users.noreply.github.com>
2024-07-13 01:23:36 +0400

    main.cpp

--- shared_ptr/main.cpp
+++ shared_ptr/main.cpp
@@ -6,14 +6,11 @@ int main() {
 
     weak_ptr<int> wp1 = sp1;
     std::cout << "wp1 lock use count: " << wp1.lock().use_count() << std::endl;
-	
-	  std::cout << "sp1 use_count after locking wp1: " << sp1.use_count() << std::endl;
+    std::cout << "sp1 use_count after locking wp1: " << sp1.use_count() << std::endl;
 
     shared_ptr<int> sp2 = wp1.lock();
     std::cout << "sp2 use count: " << sp2.use_count() << std::endl;
-
     std::cout << "sp1 reset, sp2 use count: " << sp2.use_count() << std::endl;
-
     std::cout << "sp2 reset, wp1 lock use count: " << wp1.lock().use_count() << std::endl;
 
     shared_ptr<int> sp3(new int(84));
@@ -24,4 +21,33 @@ int main() {
     shared_ptr<int> sp5;
     sp5 = std::move(sp4);
     std::cout << "sp4 use count after move assignment: " << sp4.use_count() << std::endl;
-    std::cout << "sp5 use count after move assignment: " << sp
+    std::cout << "sp5 use count after move assignment: " << sp5.use_count() << std::endl;
+
+    shared_ptr<int> sp6 = sp5;
+    std::cout << "sp5 use count after copy: " << sp5.use_count() << std::endl;
+    std::cout << "sp6 use count after copy: " << sp6.use_count() << std::endl;
+
+    shared_ptr<int> sp7;
+    sp7 = sp6;
+    std::cout << "sp6 use count after copy assignment: " << sp6.use_count() << std::endl;
+    std::cout << "sp7 use count after copy assignment: " << sp7.use_count() << std::endl;
+
+    weak_ptr<int> wp2 = wp1;
+    std::cout << "wp2 lock use count: " << wp2.lock().use_count() << std::endl;
+
+    weak_ptr<int> wp3 = std::move(wp1);
+    std::cout << "wp1 lock use count after move: " << wp1.lock().use_count() << std::endl;
+    std::cout << "wp3 lock use count after move: " << wp3.lock().use_count() << std::endl;
+
+    weak_ptr<int> wp4;
+    wp4 = std::move(wp3);
+    std::cout << "wp3 lock use count after move assignment: " << wp3.lock().use_count() << std::endl;
+    std::cout << "wp4 lock use count after move assignment: " << wp4.lock().use_count() << std::endl;
+
+    sp5 = nullptr;
+    sp6 = nullptr;
+    sp7 = nullptr;
+    std::cout << "All shared_ptrs reset, wp4 lock use count: " << wp4.lock().use_count() << std::endl;
+
+    return 0;
+}
